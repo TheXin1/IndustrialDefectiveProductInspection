df0f7773d8ac10f146667bde1a5fcd52a7c31243
TheXin1 <280337838qq.com>
2026-03-09 15:41:18 +0800

    1

---
+++ src/main/java/com/example/industrialdefectiveproductinspection/IndustrialDefectiveProductInspectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.industrialdefectiveproductinspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialDefectiveProductInspectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustrialDefectiveProductInspectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/industrialdefectiveproductinspection/IndustrialDefectiveProductInspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.industrialdefectiveproductinspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialDefectiveProductInspectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
